16982b680eba1e3738fe8c09f2be349165ebad59
Botxan <oihan.ic@gmail.com>
2022-03-06 12:36:57 +0100

    Added zulu as JRE. Removed unnecesary 'serial' warning suppress.

--- BetAndRuin2022/src/domain/Question.java
+++ BetAndRuin2022/src/domain/Question.java
@@ -8,8 +8,6 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlIDREF;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
-
-@SuppressWarnings("serial")
 @XmlAccessorType(XmlAccessType.FIELD)
 @Entity
 public class Question implements Serializable {
