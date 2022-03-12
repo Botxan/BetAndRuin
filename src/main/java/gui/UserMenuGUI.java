d38957ae50d5e8ed8e66b441772580d2cf2c464c
Botxan <oihan.ic@gmail.com>
2022-03-12 11:32:41 +0100

    Added UserMenuGUI. Closes #19
    UserMenuGUI is a modification of @pablobec93 's MenuGUI

---
+++ src/main/java/gui/UserMenuGUI.java
@@ -0,0 +1,133 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.EmptyBorder;
+
+import businessLogic.BlFacade;
+import businessLogic.BlFacadeImplementation;
+
+public class UserMenuGUI extends JFrame {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private BlFacade businessLogic;
+
+	private JPanel contentPane;
+	private JComboBox<String> localeCB;
+	private JButton browseQuestionsBtn;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UserMenuGUI frame = new UserMenuGUI(new BlFacadeImplementation());
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public UserMenuGUI(BlFacade bl) {
+		businessLogic = (BlFacade)bl;
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 398, 146);
+		
+		initializeMenuPane();
+		
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(localeCB, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 56, GroupLayout.PREFERRED_SIZE)
+						.addComponent(browseQuestionsBtn, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 362, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(9)
+					.addComponent(localeCB, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(browseQuestionsBtn, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(140, Short.MAX_VALUE))
+		);
+		getContentPane().setLayout(groupLayout);
+	}
+	
+	public void initializeMenuPane() {
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		initializeBrowseQuestionsBtn();
+		initializeLocaleCB();
+	}
+	
+	/**
+	 * This method initializes the combo box with the languages.
+	 */
+	public void initializeLocaleCB() {
+		localeCB = new JComboBox<String>();
+		
+		localeCB.addItem("EN");
+		localeCB.addItem("EUS");
+		localeCB.addItem("ES");
+		
+		localeCB.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String language = localeCB.getSelectedItem().toString();
+				Locale.setDefault(new Locale(language));
+				System.out.println("Locale: " + Locale.getDefault());
+				redraw();
+			}
+		});	
+	}
+	
+	/**
+	 * This method initializes the method for browsing questions.
+	 */
+	public void initializeBrowseQuestionsBtn() {
+		browseQuestionsBtn = new JButton();
+		browseQuestionsBtn.setText(ResourceBundle.getBundle("Etiquetas").getString("BrowseQuestions"));
+		
+		browseQuestionsBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				BrowseQuestionsGUI browseQuestionsWindow = new BrowseQuestionsGUI(businessLogic);
+				browseQuestionsWindow.setVisible(true);
+				dispose();
+			}
+		});
+	}
+	
+	/**
+	 * This method refreshes all the text fields in the GUI. It is used when the
+	 * language of the application is switched.
+	 */
+	private void redraw() {
+		browseQuestionsBtn.setText(ResourceBundle.getBundle("Etiquetas").getString("BrowseQuestions"));		
+	}
+}
