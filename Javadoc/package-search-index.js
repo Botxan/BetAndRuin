3e90ede888c2b8ae019ded78e4fb78667bc23def
Botxan <oihan.ic@gmail.com>
2022-03-18 23:13:43 +0100

    Removed eclipse with swing project

--- Javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"businessLogic"},{"l":"configuration"},{"l":"dataAccess"},{"l":"domain"},{"l":"exceptions"},{"l":"gui"},{"l":"gui.components"}];updateSearchResults();
